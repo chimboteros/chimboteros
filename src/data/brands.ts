f72f6c1965af2b26e82d7d82ed530350bd9d2f84
Joan Gavelán <joangavelan@gmail.com>
2022-09-26 22:14:48 -0500

    add creaciones section

---
+++ src/data/brands.ts
@@ -0,0 +1,99 @@
+export const brands = [
+  {
+    year: 2016,
+    name: 'fotear',
+    slogan: 'fotografía cultural',
+    description:
+      'Recorrimos nuestra provincia para investigar sus riquezas y compartirlas con la comunidad.',
+    link: 'https://www.facebook.com/FotearPeru',
+    classes: {
+      container:
+        'bg-[url(/images/mobile/fotear.jpg)] lg:bg-[url(/images/fotear.jpg)]',
+      small: 'text-[#FE0066] border-[#FE0066]',
+      content: 'text-[#1A1A1A]',
+      cta: 'text-[#FE0066]'
+    }
+  },
+  {
+    year: 2018,
+    name: 'gustar',
+    slogan: 'sabor cultural',
+    description:
+      'Investigamos la cadena de valor de nuestra gastronomía para impulsar la innovación culinaria hacia el turismo.',
+    link: 'https://www.facebook.com/GustarPeru',
+    classes: {
+      container:
+        'bg-[url(/images/mobile/gustar.jpg)] lg:bg-[url(/images/gustar.jpg)]',
+      small: 'text-[#EFFD34] border-[#EFFD34]',
+      content: 'text-[#FFFFFF]',
+      cta: 'text-[#EFFD34]'
+    }
+  },
+  {
+    year: 2018,
+    name: 'viva',
+    slogan: 'historia cultural',
+    description:
+      'Viajamos en el tiempo para desvelar las rutas de la enigmatica historia viva de Chimbote.',
+    link: 'https://www.facebook.com/vivalahistoria',
+    classes: {
+      container:
+        'bg-[url(/images/mobile/viva.jpg)] lg:bg-[url(/images/viva.jpg)]',
+      small: 'text-[#CD9632] border-[#CD9632]',
+      content: 'text-[#1A1A1A]',
+      cta: 'text-[#CD9632]'
+    }
+  },
+  {
+    year: 2021,
+    name: 'internautas',
+    slogan: 'tecnología cultural',
+    description:
+      'Unamos Chimbote desde el autodescubrimiento de sus riquezas, la creación de cultura y el desarrollo del turismo.',
+    link: 'https://www.facebook.com/Internautasperu',
+    classes: {
+      container:
+        'bg-[url(/images/mobile/internautas.jpg)] lg:bg-[url(/images/internautas.jpg)]',
+      small: 'text-[#07CBFF] border-[#07CBFF]',
+      content: 'text-[#1A1A1A] lg:text-[#FFFFFF]',
+      cta: 'text-[#07CBFF]'
+    }
+  },
+  {
+    name: 'lindo',
+    slogan: 'arte popular',
+    description:
+      'Promovemos la creación artística para transformar el talento en productos inspiradores de una industria cultural.',
+    classes: {
+      container:
+        'bg-[url(/images/mobile/lindo.jpg)] lg:bg-[url(/images/lindo.jpg)]',
+      small: 'text-[#EFFD34] border-[#EFFD34]',
+      content: 'text-[#FFFFFF]',
+      cta: 'text-[#EFFD34]'
+    }
+  },
+  {
+    name: 'aldea',
+    slogan: 'arquitectura cultural',
+    description:
+      'Soñamos con ciudades cuyos espacios sean en sí mismos experiencias turísticas por amar.',
+    classes: {
+      container: `bg-[url(/images/mobile/aldea.jpg)] lg:bg-[url(/images/aldea.jpg)]`,
+      small: 'text-[#29E0E1] border-[#29E0E1]',
+      content: 'text-[#FFFFFF]',
+      cta: 'text-[#29E0E1]'
+    }
+  },
+  {
+    name: 'crear',
+    slogan: 'emprende cultural',
+    description:
+      'Forjemos riqueza para el cuerpo y el alma, para la economía y el ambiente, para la vida.',
+    classes: {
+      container: `bg-[url(/images/mobile/crear.jpg)] lg:bg-[url(/images/crear.jpg)]`,
+      small: 'text-[#8440B3] border-[#8440B3]',
+      content: 'text-[#FFFFFF]',
+      cta: 'text-[#8440B3]'
+    }
+  }
+]
---
+++ src/data/timelineBrands.ts
@@ -0,0 +1,46 @@
+export const timelineBrands = [
+  {
+    name: 'fotear',
+    field: 'fotografía',
+    year: 2016
+  },
+  {
+    name: 'gustar',
+    field: 'gastronomía',
+    year: 2018
+  },
+  {
+    name: 'viva',
+    field: 'historia',
+    year: 2019
+  },
+  {
+    name: 'internautas',
+    field: 'tecnología',
+    year: 2021
+  },
+  {
+    name: 'lindo',
+    field: 'arte'
+  },
+  {
+    name: 'aldea',
+    field: 'arquitectura'
+  },
+  {
+    name: 'crear',
+    field: 'emprendimiento'
+  },
+  {
+    name: 'natural',
+    field: 'ecología'
+  },
+  {
+    name: 'mírame',
+    field: 'cinema'
+  },
+  {
+    name: 'súper',
+    field: 'bienestar'
+  }
+]
