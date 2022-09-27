06ae4ad23a0c38d65be5f779fc4755f73a573d84
Joan Gavelán <joangavelan@gmail.com>
2022-09-27 16:40:29 -0500

    add products section

---
+++ src/data/products.ts
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    name: 'proyectos',
+    description: 'diseño e implementación',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/produccion-del-proyecto-gustar.jpg)] lg:bg-[url(/images/produccion-del-proyecto-gustar.jpg)]',
+      text: 'text-white',
+      position: 'self-end'
+    }
+  },
+  {
+    name: 'fotogalerías',
+    description: 'Tesoros Escondidos',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/exposicion-de-galeria-proyecto-fotear.jpg)] lg:bg-[url(/images/exposicion-de-galeria-proyecto-fotear.jpg)]',
+      text: 'text-white',
+      position: 'self-end'
+    }
+  },
+  {
+    name: 'festivales',
+    description: 'Innovación Cultural',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/festival-de-la-ruta-del-ceviche.jpg)] lg:bg-[url(/images/festival-de-la-ruta-del-ceviche.jpg)]',
+      text: 'text-white',
+      position: 'self-start'
+    }
+  },
+  {
+    name: 'enciclopedia',
+    description: 'Desafíos y oportunidades',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/enciclopedia.jpg)] lg:bg-[url(/images/enciclopedia.jpg)]',
+      text: 'text-white',
+      position: 'self-end'
+    }
+  },
+  {
+    name: 'aplicaciones',
+    description: 'Uniendo los valores de la gente y sus riquezas',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/red-social-culturalismo.jpg)] lg:bg-[url(/images/smartphones.jpg)]',
+      text: 'text-white lg:text-[#1A1A1A]',
+      position: 'self-start'
+    }
+  },
+  {
+    name: 'documentales',
+    description: 'Las buenas historias de Chimbote',
+    classes: {
+      background:
+        'bg-[url(/images/mobile/portada-de-documentales.jpg)] lg:bg-[url(/images/pescador-chimbotero.jpg)]',
+      text: 'text-white',
+      position: 'self-end lg:self-start'
+    }
+  }
+]
