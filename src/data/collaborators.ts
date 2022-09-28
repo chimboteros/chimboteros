515be0f73a262255a92354619f9413aac0ab0b02
Joan Gavelán <joangavelan@gmail.com>
2022-09-27 20:05:44 -0500

    add team section

---
+++ src/data/collaborators.ts
@@ -0,0 +1,14 @@
+export const collaborators = [
+  'Luis Mariños Cabrera',
+  'Renzo Lomparte Sánchez',
+  'Rosario Sánchez Villegas',
+  'Sammira Torres López',
+  'Venicia Quiroz Carrillo',
+  'Analiza Fiestas Salapi',
+  'Estefania Vásquez Sánchez',
+  'William Quispe Rojas',
+  'Kevin Ramos Núñez',
+  'Karla Ortega Díaz',
+  'Yeilimer Rosales Soto',
+  'Jarumi Lázaro Huiza'
+]
---
+++ src/data/team.ts
@@ -0,0 +1,62 @@
+export const teamMembers = [
+  {
+    name: 'david aguedo',
+    field: 'dirección',
+    summary:
+      'Administrador, diseñador y gestor cultural con 15 años de experiencia en el mundo del branding. Responsable de conceptualizar y dirigir los proyectos, integrando cultura, producción y turismo.'
+  },
+  {
+    name: 'joan gavelán',
+    field: 'desarrollo',
+    summary:
+      'Promotor cultural y desarrollador digital con 5 años de experiencia en proyectos de artes visuales y comunicación digital. Desde el 2020 es responsable del equipo desarrollador de la Plataforma Digital Chimboteros.'
+  },
+  {
+    name: 'jazmín cuba',
+    field: 'educación',
+    summary:
+      'Psicóloga y promotora cultural con 6 años de experiencia en educación creativa. Desde el 2017, responsable de los talleres de capacitación dirigidos a los participantes y colaboradores de los proyectos de la organización.'
+  },
+  {
+    name: 'thony juárez',
+    field: 'videografía',
+    summary:
+      'Promotor cultural y fotógrafo profesional con 5 años de experiencia en proyectos de artes visuales. Desde el 2018, videógrafo responsable de la gestión de los contenidos visuales de la organización.'
+  },
+  {
+    name: 'carlos sánchez',
+    field: 'fotografía',
+    summary:
+      'Comunicador social, relacionista público y fotógrafo con 17 años de experiencia en proyectos que promueven la identidad cultural. Desde el 2019, responsable de gestionar los contenidos fotográficos de la organización.'
+  },
+  {
+    name: 'josé luis pinedo',
+    field: 'gastronomía',
+    summary:
+      'Cocinero profesional con 4 años de experiencia promoviendo la innovación culinaria hacia el turismo Desde el 2020, responsable de la investigación gastronómica y alimentaria en la provincia del Santa.'
+  },
+  {
+    name: 'toño castillo',
+    field: 'turismo',
+    summary:
+      'Comunicador social y promotor turístico con 13 años de experiencia recorriendo los recursos de la provincia del Santa. Desde el 2017, responsable de los proyectos turísticos desde la Comunicación para el Desarrollo.'
+  },
+  {
+    name: 'ángel zavaleta',
+    field: 'diseño',
+    summary:
+      'Diseñador y promotor cultural. Desde el presente año, responsable de gestionar los diseños y sus respectivas aplicaciones de comunicación en todo los proyectos de la organización.'
+  },
+  {
+    name: 'lucía rojas',
+    field: 'dirección',
+    summary:
+      'Psicóloga y gestora cultural, con 4 años de experiencia en proyectos artísticos de artesanía sobre patrimonios arqueológicos y naturales. Desde el 2019, responsable de gestionar los proyectos de arte popular.'
+  },
+  {
+    name: 'kary arias',
+    field: 'comunicación',
+    summary:
+      'Comunicadora social y promotora cultural con 5 años de experiencia en proyectos audiovisuales en cultura y turismo. Desde el presente año, responsable de la estrategia de comunicación de los proyectos de la organización.'
+  }
+]
